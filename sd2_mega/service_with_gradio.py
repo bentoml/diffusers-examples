e96c65755efcbb70c9ea82dc0888e9905782bff3
Zhao Shenyang <dev@zsy.im>
2023-04-18 03:49:27 +0800

    pretty print css

--- sd2_mega/service_with_gradio.py
+++ sd2_mega/service_with_gradio.py
@@ -55,7 +55,31 @@ def inference(prompt, guidance, steps, img=None, width=512, height=512, strength
         )
         return images
 
-css = """.finetuned-diffusion-div div{display:inline-flex;align-items:center;gap:.8rem;font-size:1.75rem}.finetuned-diffusion-div div h1{font-weight:900;margin-bottom:7px}.finetuned-diffusion-div p{margin-bottom:10px;font-size:94%}a{text-decoration:underline}.tabs{margin-top:0;margin-bottom:0}#gallery{min-height:20rem}
+css = """
+.finetuned-diffusion-div div{
+    display:inline-flex;
+    align-items:center;
+    gap:.8rem;
+    font-size:1.75rem
+}
+.finetuned-diffusion-div div h1{
+    font-weight:900;
+    margin-bottom:7px
+}
+.finetuned-diffusion-div p{
+    margin-bottom:10px;
+    font-size:94%
+}
+a{
+    text-decoration:underline
+}
+.tabs{
+    margin-top:0;
+    margin-bottom:0
+}
+#gallery{
+    min-height:20rem
+}
 """
 
 with gr.Blocks(css=css) as demo:
